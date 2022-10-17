ee55bf86cfae1c12d83eb53a7a8ac5b2f154f340
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-10-17 13:05:21 -0300

    adiciona o componente populares

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -2,23 +2,25 @@ import Banner from "../../componentes/Banner";
 import Cabecalho from "../../componentes/Cabecalho";
 import Galeria from "../../componentes/Galeria";
 import Menu from "../../componentes/Menu";
+import Populares from "../../componentes/Populares";
 import Rodape from "../../componentes/Rodape";
 import styles from './PaginaInicial.module.scss'
 
 export default function PaginaInicial() {
-    return (
-        <>
-          <Cabecalho />
-          <main>
-            <section className={styles.principal}>
-              <Menu />
-              <Banner />
-            </section>
-          </main>
-          <div className={styles.galeria}>
-              <Galeria />
-          </div>
-          <Rodape />
-        </>
-    )
+  return (
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <Banner />
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
+      <Rodape />
+    </>
+  )
 }
